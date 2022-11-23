7dff624a2aeda1004179553263d748b5563580e7
Adi <valcuadrian90@yahoo.com>
2022-11-23 10:42:41 +0200

    mods4

--- src/components/price/Price.js
+++ src/components/price/Price.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Price = (props) => {
-  return <span class="details">{props.label}</span>;
+  return <span class="price">{props.label}</span>;
 };
 
 export default Price;
